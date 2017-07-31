0e8a6dff917775b890f8e4da31e21f9cc7dbbd0e
BazingoW <ramiro.animus@gmail.com>
2017-07-31 02:09:29 +0100

    final touches 1

--- Assets/DiaLogManager.cs
+++ Assets/DiaLogManager.cs
@@ -44,30 +44,30 @@ public class DiaLogManager : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.K)) {
 
-			if(dialogN>dialog.Length-1)
+			if (dialogN > dialog.Length - 1)
 				SceneManager.LoadScene (nextScene);
+			else {
+				if (dialog [dialogN].person == 0) {
+					sticks [0].SetActive (true);
+					sticks [1].SetActive (false);
+					speech.transform.position = pos [0].position;
 			
-			if (dialog [dialogN].person == 0) {
-				sticks [0].SetActive (true);
-				sticks [1].SetActive (false);
-				speech.transform.position = pos [0].position;
-			
-			} else {
-				sticks [0].SetActive (false);
-				sticks [1].SetActive (true);
-				speech.transform.position = pos [1].position;
+				} else {
+					sticks [0].SetActive (false);
+					sticks [1].SetActive (true);
+					speech.transform.position = pos [1].position;
+				}
+
+				audioS.clip = talks [dialog [dialogN].soundId];
+				audioS.Play ();
+
+				speech.text = dialog [dialogN].talk;
+				if (dialogN == 1) {
+					robot.SetActive (true);
+				}
+				dialogN++;
+
 			}
-
-			audioS.clip = talks [dialog [dialogN].soundId];
-			audioS.Play();
-
-			speech.text = dialog [dialogN].talk;
-			if (dialogN == 1) {
-				robot.SetActive (true);
-			}
-			dialogN++;
-
-
 
 		}
 	}
